10550cca29960184c92490fc4a227a62b5ffc972
souznerushimiu <zelnastik@555gmail.com>
2024-07-15 20:55:33 +1000

    dz4

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-// let res = document.querySelector("#text_2");
-// console.log(res);
+let imgTime =["c0.gif", "c1.gif", "img/c2.gif", "c3.gif", "c4.gif", "c5.gif", "c6.gif", "c7.gif", "c8.gif", "c9.gif"];
 
-// let res2 = document.querySelectorAll("#text_2");
-// console.log(res2);
+let t = document.querySelector("#clock img");
 
-// let el = document.querySelector("h2");
-// console.log(el.innerHTML);
+clock();
 
-// let els = document.querySelectorAll("h2");
-// console.log(els[1].innerHTML);
+function clock(){
+    let time = new Date();
+    let hour = time.getHours;
+    let min = time.getMinutes;
+    let sec = time.getSeconds;
+    console.log(hour, min, sec);
+    setTimeout(clock, 1000);
+}
 
-// let lists = document.querySelectorAll("li");
+function getImg(h, m, s){
+    t[0].src = imgTime[parseInt(h/10)];
+    t[1].src = imgTime[h%10];
 
-// for(let i = 0; i<lists.length; i++){
-//     lists[i].innerHTML += "!!!";
+    
 
-// }
-
-// let m = document.querySelectorAll(".purple li")[1];
-
-// function test(a,b,c) {
-//     let res = a + b + c;
-//     return res;
-// }
-// alert(test(10,20,30));
-
-// let test2 = (a,b,c) => a + b + c;
-// alert(test2(1,2,3));
-
-// let hello = (n, m) => alert("Hello" + n);
-// hello("Igor");
-
-// let hello = () => {
-//     if(n){
-//         return"Hello";
-//     }
-//     return"Привет";
-// }
-// alert(hello(0));
-
-console.log(Math.floor(7.9));
-console.log(Math.ceil(7.1));
-console.log(Math.round(7.5));
+}
