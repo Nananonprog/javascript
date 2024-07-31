eb385e111b7346b9e9c191afba27107ea68378f5
souznerushimiu <zelnastik@555gmail.com>
2024-07-31 15:00:19 +1000

    dz8

---
+++ dz8/script.js
@@ -0,0 +1,20 @@
+document.querySelector("#load").addEventListener("click", load);
+function load(){
+    let url = "https://jsonplaceholder.typicode.com/todos";
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let ul = document.querySelector("#list");
+            let html = data.map(function(item){
+                if(item.completed){
+                return "<li>" + "Пользователь: " +  item.userId + "выполнил задачу № " + item.id + " (" + item.title + ")" +  "</li>";}
+                
+
+            })
+            ul.insertAdjacentHTML("afterbegin", html.join(" "));
+        })
+
+}
+// return "<li>" + "Пользователь :" + item.userId + "выполнил задачу № " + item.id + " (" + item.title + ")" + "</li>";
